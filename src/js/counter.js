997b885cd10dc5a8a21449f830bcfb00cf1b5b6b
A-Harta <antonbog000@gmail.com>
2025-07-12 21:32:03 +0300

    Task-1. Удалит стили swiper

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,1 +1,14 @@
-// your code
+import { SELECTORS } from './selectors.js'
+
+// счетчик добавить
+if (SELECTORS) {
+  SELECTORS?.counterBtnUp?.addEventListener('click', () => {
+    console.log('click')
+  })
+}
+// счетчик убавить
+if (SELECTORS) {
+  SELECTORS?.counterBtnDown?.addEventListener('click', () => {
+    console.log('click')
+  })
+}
