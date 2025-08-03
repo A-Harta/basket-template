b4b6c35bcd5de35ca4f2049e3638c03a5cf663b5
A-Harta <antonbog000@gmail.com>
2025-08-03 18:28:44 +0300

    Открытие корзины товаров в сайдбаре. Кореектировка компонетов Notification. Привязка обработчиков событий

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 import { Sidebar } from './components/sidebar.js'
 import { getProducts } from './api.js'
 import { generateTemplate } from './templates.js'
-import { SELECTORS } from './global.js'
+import { SELECTORS, PRODUCTS_IN_BASKET } from './global.js'
 import { createNewProduct } from './handlers.js'
+import { renderBasket } from './templates.js'
 
 window.addEventListener('DOMContentLoaded', async () => {
   // Получение продуктов
@@ -11,8 +12,14 @@ window.addEventListener('DOMContentLoaded', async () => {
   // Формирование шаблона
   generateTemplate(products, SELECTORS?.productsList)
 
-  // Вызываем сайдбар
+  // Рендерим корзину
+  renderBasket(PRODUCTS_IN_BASKET, SELECTORS?.basketList)
+
+  // Вызываем сайдбар (основной)
   new Sidebar('#sidebar', '#sidebar-open')
+
+  // Вызываем сайдбар (корзина)
+  new Sidebar('#basket-sidebar', '#basket-button')
 
   // Навешиваем обработчик на форму добавления товара
   createNewProduct()
