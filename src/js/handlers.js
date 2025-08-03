da2c70e4562257f6a76d864478a4f127b182ceb9
A-Harta <antonbog000@gmail.com>
2025-08-03 17:37:32 +0300

    Чистка неиспользуемых методов, импортов, переменных

---
+++ src/js/handlers.js
@@ -0,0 +1,57 @@
+import { SELECTORS } from './global'
+import { createProduct } from './api'
+import { generateTemplate } from './templates'
+import { Notification } from './components/notification'
+import { PRODUCTS_IN_BASKET } from './global'
+import { renderBasket } from './templates'
+
+// Функция добавления продукта через форму
+export const createNewProduct = async () => {
+  if (SELECTORS?.addProductForm) {
+    SELECTORS?.addProductForm?.addEventListener('submit', async (event) => {
+      event.preventDefault()
+      const form = event.target
+
+      const product = {
+        name: form.name.value,
+        rating: Number.parseInt(form.rating.value, 10),
+        price: Number.parseInt(form.price.value, 10),
+        category: form.category.value,
+        imgSrc: form.imgSrc.value,
+        description: form.description.value,
+      }
+
+      // Создаём продукт
+      const newProduct = await createProduct(product)
+
+      // Вставляем карточку на главную страницу
+      generateTemplate(newProduct, SELECTORS?.productsList)
+
+      form.reset()
+    })
+  }
+}
+
+// Функция добавления товара в корзину
+export const addToBasket = (product) => {
+  const exists = PRODUCTS_IN_BASKET?.some((item) => item?.id === product?.id)
+  if (!exists) PRODUCTS_IN_BASKET.push(product)
+  renderBasket(PRODUCTS_IN_BASKET, SELECTORS?.basketList)
+
+  new Notification({
+    title: 'Добавление товара',
+    subtitle: 'Товар добавлен в корзину',
+  })
+}
+
+// Функция удаления товара из корзины
+export const removeFromBasket = (carId) => {
+  const index = PRODUCTS_IN_BASKET?.findIndex((item) => item?.id === carId)
+  if (index !== -1) PRODUCTS_IN_BASKET.splice(index, 1)
+  renderBasket(PRODUCTS_IN_BASKET, SELECTORS?.basketList)
+
+  new Notification({
+    title: 'Удаление товара',
+    subtitle: 'Товар удален из корзины',
+  })
+}
